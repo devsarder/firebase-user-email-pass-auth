d1406573e8ef595abf2511425aa236a6ea61ec16
sujon sarder <SUJONSARDER92@GMAIL.COM>
2023-10-01 12:38:03 +0200

    registration and login form are linked

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,24 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import auth from "../../firebase/firbase.init";
+import { Link } from "react-router-dom";
+
 const Login = () => {
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+    e.target.reset();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log(user);
+      })
+      .then((error) => console.log(error));
+  };
   return (
     <div>
-      <form className="flex flex-col gap-1 max-w-lg">
+      <form onSubmit={handleSignIn} className="flex flex-col gap-1 max-w-lg">
         <h3 className="text-2xl">Please Login</h3>
         <input
           className="bg-gray-200 px-2 py-1 rounded-md"
@@ -21,6 +38,11 @@ const Login = () => {
         <br />
         <input className="btn btn-success" type="submit" value="Log in" />
       </form>
+      <div>
+        <p>
+          New to this website? Please <Link to="/register">Register</Link>
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,17 +1,25 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../firebase/firbase.init";
 import { useState } from "react";
+import { FaRegEyeSlash, FaRegEye } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const [registerError, setRegisterError] = useState("");
   const [success, setSuccess] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleRegistration = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
+    const terms = e.target.terms.checked;
+    e.target.reset();
     if (password.length < 6) {
       setRegisterError("password too short should be 6 characters or longer");
+      return;
+    } else if (!terms) {
+      setRegisterError("Please accept our terms and conditions");
       return;
     }
     setRegisterError("");
@@ -47,19 +55,43 @@ const Register = () => {
         <br />
         <input
           className="bg-gray-200 px-2 py-1 rounded-md"
-          type="password"
+          type={showPassword ? "text" : "password"}
           name="password"
           id=""
           placeholder="Password"
           required
         />
+        <span
+          onClick={() => setShowPassword(!showPassword)}
+          className="btn btn-primary rounded-md p-2"
+        >
+          {showPassword ? (
+            <FaRegEye className="text-xl"></FaRegEye>
+          ) : (
+            <FaRegEyeSlash className="text-xl"></FaRegEyeSlash>
+          )}
+        </span>
         <br />
+        <div>
+          <input type="checkbox" name="terms" id="terms" />
+          <label htmlFor="terms">
+            Please Accept our
+            <a className="underline" href="">
+              Terms and conditions
+            </a>
+          </label>
+        </div>
         <input className="btn btn-primary" type="submit" value="submit" />
       </form>
       <div>
         {registerError && <p className="text-red-600">{registerError}</p>}
       </div>
       <div>{success && <p className="text-green-600">{success}</p>}</div>
+      <div>
+        <p>
+          Already Have An Account? Please <Link to="/login">Login</Link>
+        </p>
+      </div>
     </div>
   );
 };
